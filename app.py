723066d0b4b742a2ac2529de05fd9fa982de7ef4
Dan Cuadra <dan.cua3337@vagaro.com>
2023-06-27 10:23:47 -0700

    attempting to push gpt app base line

--- app.py
+++ app.py
@@ -1,15 +1,54 @@
 import os
-
+from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain.chat_models import ChatOpenAI
+import gradio as gr
+import pandas as pd
+import sys
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+os.environ["OPENAI_API_KEY"] = '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'
 
 @app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+#def construct_index(directory_path):
+#    max_input_size = 4096
+#    num_outputs = 512
+#    max_chunk_overlap = 50
+#    chunk_size_limit = 600
+
+#    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+#    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+
+#    documents = SimpleDirectoryReader(directory_path).load_data()
+
+#    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+#    index.save_to_disk('index.json')
+
+#    return index
+
+#df['ada_embedding'] = df.ada_embedding.apply(eval).apply(np.array)
+
+def chatbot(input_text):
+    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+    response = index.query(input_text, response_mode="compact")
+    return response.response
+
+
+
+
+#Not necessary in live code (gradio as gr)
+iface = gr.Interface(fn=chatbot,
+                     inputs=gr.components.Textbox(lines=7, label="Enter your text"),
+                     outputs="text",
+                     title="Custom-trained AI Chatbot")
+
+#index = construct_index("docs")
+iface.launch(share=True)
+
 
 @app.route('/favicon.ico')
 def favicon():
@@ -26,7 +65,6 @@ def hello():
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
    app.run()
